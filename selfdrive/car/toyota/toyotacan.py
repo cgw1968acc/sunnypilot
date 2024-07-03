04db2c23a11d7cab7de92641f621307017ef4442
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-07-03 22:41:13 +0800

    Update toyotacan.py

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -160,7 +160,7 @@ def create_brake_hold_command(packer, frame, pre_collision_2, brake_hold_active)
   if brake_hold_active:
     values = {
       "DSS1GDRV": 0x3FF,
-      "PBRTRGR": frame % 730 < 724,  # cut actuation for 6 frames
+      "PBRTRGR": frame % 730 < 727,  # cut actuation for 3 frames
     }
 
   return packer.make_can_msg("PRE_COLLISION_2", 0, values)
