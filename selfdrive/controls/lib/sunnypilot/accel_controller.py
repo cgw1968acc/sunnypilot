3736b64488e2e969eb8758028cc4740ebde382f8
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-07-17 22:03:03 +0800

    Update accel_controller.py

--- selfdrive/controls/lib/sunnypilot/accel_controller.py
+++ selfdrive/controls/lib/sunnypilot/accel_controller.py
@@ -29,13 +29,13 @@ from openpilot.common.numpy_fast import interp
 AccelPersonality = custom.AccelerationPersonality
 
 # accel personality by @arne182 modified by cgw
-_DP_CRUISE_MIN_V =       [-0.40,  -0.40,  -0.62,  -0.62,  -0.72,  -0.72,  -0.88,  -0.82]
-_DP_CRUISE_MIN_V_ECO =   [-0.05,  -0.05,  -0.10,  -0.30,  -0.50,  -0.70,  -0.80,  -0.80]
-_DP_CRUISE_MIN_V_SPORT = [-0.50,  -0.50,  -0.64,  -0.64,  -0.74,  -0.74,  -0.90,  -0.84]
+_DP_CRUISE_MIN_V =       [-0.06,  -0.06,  -0.12,  -0.32,  -0.52,  -0.72,  -0.88,  -0.82]
+_DP_CRUISE_MIN_V_ECO =   [-0.04,  -0.04,  -0.10,  -0.30,  -0.50,  -0.70,  -0.80,  -0.80]
+_DP_CRUISE_MIN_V_SPORT = [-0.08,  -0.08,  -0.14,  -0.34,  -0.54,  -0.74,  -0.90,  -0.84]
 _DP_CRUISE_MIN_BP =      [0.,     0.05,   5.56,    8.33,    11.1,   16.,    30.,    40.]
 
 _DP_CRUISE_MAX_V =       [2.0, 2.0, 2.0, 1.3,  .80,  .68,  .55,  .42,  .348, .12]
-_DP_CRUISE_MAX_V_ECO =   [1.6, 1.6, 1.4, 1.1,  .70,  .58,  .45,  .32,  .20,  .085]
+_DP_CRUISE_MAX_V_ECO =   [1.6, 1.6, 1.4, 1.1,  .70,  .58,  .45,  .34,  .26,  .085]
 _DP_CRUISE_MAX_V_SPORT = [2.4, 2.4, 2.2, 1.6,  1.1,  .84,  .89,  .75,  .50,  .2]
 _DP_CRUISE_MAX_BP =      [0.,  1.,  6.,  8.,   11.,  15.,  20.,  25.,  30.,  55.]
 
