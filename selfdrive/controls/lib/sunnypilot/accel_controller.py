166305616039a38f5e63b25d3cd4f881fe1a3802
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-08-03 00:46:44 +0800

    Update accel_controller.py

--- selfdrive/controls/lib/sunnypilot/accel_controller.py
+++ selfdrive/controls/lib/sunnypilot/accel_controller.py
@@ -29,10 +29,10 @@ from openpilot.common.numpy_fast import interp
 AccelPersonality = custom.AccelerationPersonality
 
 # accel personality by @arne182 modified by cgw
-_DP_CRUISE_MIN_V =       [-1.0, -0.80]
-_DP_CRUISE_MIN_V_ECO =   [-1.0, -0.76]
-_DP_CRUISE_MIN_V_SPORT = [-1.0, -0.84]
-_DP_CRUISE_MIN_BP =      [0.,   20.]
+_DP_CRUISE_MIN_V =       [-1.0, -1.0,  -0.80]
+_DP_CRUISE_MIN_V_ECO =   [-1.0, -1.0,  -0.76]
+_DP_CRUISE_MIN_V_SPORT = [-1.0, -1.0,  -0.84]
+_DP_CRUISE_MIN_BP =      [0.,   5.56,  20.]
 
 _DP_CRUISE_MAX_V =       [2.0, 2.0, 2.0, 1.68, 1.25, .72,  .53,  .42,  .13]
 _DP_CRUISE_MAX_V_ECO =   [2.0, 2.0, 2.0, 1.38, 0.91, .53,  .42,  .31,  .085]
