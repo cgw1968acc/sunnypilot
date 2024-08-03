cef7f0576792ecada84642283415cdabe3a4f1d2
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-08-04 01:11:10 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -86,17 +86,17 @@ def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
 
 def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    x_vel =  [0,    11,   14.5, 15,   20,   20.01,  25,    25.01,  36,  36.01]
-    y_dist = [1.5,  1.5,  1.5,  1.6,  1.76, 1.76,   1.78,  1.78,   1.8, 1.8]
+    x_vel =  [0,    20.,   27.7]
+    y_dist = [1.75, 1.75,  2.00]
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    x_vel =  [0,    11,   14.5, 15,   20,   20.01,  25,    25.01,  36,  36.01]
-    y_dist = [1.20, 1.20, 1.40, 1.50, 1.60, 1.76,   1.76,  1.78,   1.8, 1.8]
+    x_vel =  [0,    20.,   27.7]
+    y_dist = [1.75, 1.75,  2.00]
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    x_vel =  [0,    11,   14.5, 15,   20,   20.01,  25,    25.01,  36,  36.01]
-    y_dist = [1.0,  1.0,  1.2,  1.25, 1.35, 1.385,  1.385, 1.4,   1.4,  1.45]
+    x_vel =  [0,    20.,   27.7]
+    y_dist = [1.45, 1.45,  1.60]
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    x_vel =  [0,     5,     5.01,  11,    14.5,   15,    20,    20.01,  25, 25.01, 36,   36.01]
-    y_dist = [0.80,  0.80,  0.90,  0.90,  0.90,  1.000, 1.105, 1.15, 1.15, 1.20, 1.23,  1.23]
+    x_vel =  [0,    20.,   27.7]
+    y_dist = [1.28, 1.28,  1.35]
   else:
     raise NotImplementedError("Dynamic personality not supported")
   return np.interp(v_ego, x_vel, y_dist)
