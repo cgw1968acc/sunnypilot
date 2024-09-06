04d5422ee59b466b409568d319e45da248ab9a74
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-09-05 17:52:51 -0700

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -93,10 +93,10 @@ def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.
     y_dist = [0.0,  1.2,  1.7,  1.7,   1.75,  1.75,  1.83]
   elif personality==custom.LongitudinalPersonalitySP.standard:
     x_vel =  [0,    5.0,  5.01, 8.33,  8.34,  27.69, 27.7]
-    y_dist = [0.0,  1.2,  1.6,  1.6,   1.75,  1.75,  1.80]
+    y_dist = [0.0,  1.6,  1.6,  1.6,   1.75,  1.75,  1.80]
   elif personality==custom.LongitudinalPersonalitySP.moderate:
     x_vel =  [0,    5.,   5.01, 8.33,  8.34,  27.69, 27.7]
-    y_dist = [0.0,  1.2,  1.4,  1.4,   1.45,  1.45,  1.50]
+    y_dist = [0.0,  1.4,  1.4,  1.4,   1.45,  1.45,  1.50]
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
     x_vel =  [0,    5.,   5.01,  17.0, 17.01, 27.69, 27.7]
     y_dist = [0.0,  1.2,  1.2,   1.2,  1.25,  1.25,  1.3]
