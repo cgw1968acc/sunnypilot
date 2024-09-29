24a35ca1812cdab11f9d8b8df498cb89340be6bd
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-09-30 07:12:19 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -89,16 +89,16 @@ def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
 
 def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    x_vel =  [0,    10.,  10.01,  14.,  27.7］
+    x_vel =  [0.,   10.,  10.01,  14.,  27.7]
     y_dist = [0.6,  0.6,  1.0,    1.6,  1.80]
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    x_vel =  [0,    10.,  10.01,  14.,  27.7]
+    x_vel =  [0.,   10.,  10.01,  14.,  27.7]
     y_dist = [0.6,  0.6,  1.0,    1.4,  1.70]
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    x_vel =  [0,    10.,  10.01,  14.,  27.7]
+    x_vel =  [0.,   10.,  10.01,  14.,  27.7]
     y_dist = [0.6,  0.6,  1.0,    1.2,  1.40]
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    x_vel =  [0,    10.,  10.01,  14.,  27.7]
+    x_vel =  [0.,   10.,  10.01,  14.,  27.7]
     y_dist = [0.6,  0.6,  1.0,    1.0,  1.25]
   else:
     raise NotImplementedError("Dynamic personality not supported")
