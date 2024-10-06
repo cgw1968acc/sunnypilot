c74e3584f3401058e82c01c3e5125f1f5b6ca21b
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-10-06 10:08:55 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -163,7 +163,7 @@ class CarInterface(CarInterfaceBase):
     # hand tuned (August 12, 2024)
     def custom_tss2_longitudinal_tuning():
       ret.vEgoStopping = 0.25
-      ret.vEgoStarting = 0.15
+      ret.vEgoStarting = 0.10
       ret.stoppingDecelRate = 0.001
 
     def default_tss2_longitudinal_tuning():
