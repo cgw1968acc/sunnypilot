61e83b38893cc0564492ba33a9ada935707015c6
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-11-10 16:59:33 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -186,7 +186,7 @@ class CarInterface(CarInterfaceBase):
         #tune.kpBP = [0., 5., 20.]
         #tune.kpV = [2.3, 1.0, 0.7]
         tune.kiBP = [0.,   1.,   2.,     8.,     12.,   20.,   27.]
-        tune.kiV = [0.32,  0.33,  0.30,  0.21,  0.20,  0.17, 0.10]
+        tune.kiV = [0.35,  0.33,  0.30,  0.21,  0.20,  0.17, 0.10]
         custom_tss2_longitudinal_tuning()
       else:
         tune.kpV = [0.0]
