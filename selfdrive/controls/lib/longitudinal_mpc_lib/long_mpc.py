9a099987855cc374f3882a928bcf8233abe5f9b5
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-11-11 23:38:49 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -90,7 +90,7 @@ def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
 def get_dynamic_personality(v_ego, personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
     x_vel =  [0,    22,    22.01,  36.1]
-    y_dist = [1.70, 1.70,  1.80,   1.80]
+    y_dist = [1.50, 1.50,  1.50,   1.50]
   elif personality==custom.LongitudinalPersonalitySP.standard:
     x_vel =  [0,    22,    22.01,  36.1]
     y_dist = [1.65, 1.65,  1.75,   1.75]
