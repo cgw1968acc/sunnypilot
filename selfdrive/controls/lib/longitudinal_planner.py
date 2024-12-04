bac8792c7c80ea111066507112a911c849796734
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2024-12-04 11:31:41 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -19,7 +19,7 @@ from openpilot.common.swaglog import cloudlog
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MIN = -1.2
 
-A_CRUISE_MAX_VALS = [2.0, 2.0,  1.85, 0.926, .57, .47,  .33,  .10]
+A_CRUISE_MAX_VALS = [2.0, 2.0,  1.86, 0.926, .57, .47,  .33,  .10]
 A_CRUISE_MAX_BP =   [0.,  6.1,  8.,   11.,   20.,  25.,  30.,  40.]
 A_CRUISE_MIN_VALS = [-0.022, -0.022, -0.064, -0.064, -0.15, -0.15, -0.22,  -0.22, -1.0, -1.0]
 A_CRUISE_MIN_BP =   [0.,     3.11,   3.12,   9.,     9.01,  14.0,  14.01,  18.,   20.,  30.]
