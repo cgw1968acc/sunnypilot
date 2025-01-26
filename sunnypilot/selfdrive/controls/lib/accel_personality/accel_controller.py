2fce3f544a0d4201ff59e6447830ad989a0b0eb6
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-01-26 22:26:12 +0800

    Update accel_controller.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -30,9 +30,9 @@ AccelPersonality = custom.LongitudinalPlanSP.AccelerationPersonality
 
 # Accel personality by @arne182 modified by cgw and kumar
 
-_DP_CRUISE_MIN_V_ECO =    [-0.01, -0.01, -0.01, -0.16, -0.30, -0.30, -0.76, -0.76, -1.0,  -1.0]
-_DP_CRUISE_MIN_V_NORMAL = [-0.02, -0.02, -0.02, -0.18, -0.31, -0.31, -0.77, -0.77, -1.0,  -1.0]
-_DP_CRUISE_MIN_V_SPORT =  [-0.03, -0.03, -0.03, -0.20, -0.32, -0.32, -0.78, -0.78, -1.2,  -1.2]
+_DP_CRUISE_MIN_V_ECO =    [-0.02, -0.02, -0.02, -0.16, -0.30, -0.30, -0.76, -0.76, -1.0,  -1.0]
+_DP_CRUISE_MIN_V_NORMAL = [-0.03, -0.03, -0.03, -0.18, -0.31, -0.31, -0.77, -0.77, -1.0,  -1.0]
+_DP_CRUISE_MIN_V_SPORT =  [-0.04, -0.04, -0.04, -0.20, -0.32, -0.32, -0.78, -0.78, -1.2,  -1.2]
 _DP_CRUISE_MIN_BP =       [0.,    2.0,   2.01,  10.,   10.01, 15.,   15.01, 20.,   20.01, 40.]
 
 _DP_CRUISE_MAX_V_ECO =    [1.68, 1.68, 1.68, 1.68, 0.85, .532, .432, .32,  .28,  .085]
