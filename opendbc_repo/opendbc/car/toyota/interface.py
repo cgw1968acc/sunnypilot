ae29aeb7eb116844c74c1ab0475284d3ef404cda
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-01-30 16:34:21 +0800

    Update interface.py

--- opendbc_repo/opendbc/car/toyota/interface.py
+++ opendbc_repo/opendbc/car/toyota/interface.py
@@ -149,7 +149,7 @@ class CarInterface(CarInterfaceBase):
       if sp_tss2_long_tune:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.20
-        ret.stoppingDecelRate = 0.006  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.008  # reach stopping target smoothly
       else:
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
