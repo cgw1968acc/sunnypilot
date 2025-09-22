9af2d702fc0b0830bf9005f25e3532e72d0da32f
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-09-22 21:46:27 +0800

    Update vibe_personality.py

--- sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
+++ sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
@@ -33,7 +33,7 @@ MIN_ACCEL_BREAKPOINTS =         [0.,   1.5,    3.,   11.,   16.,  50.]
 FOLLOW_PROFILES = {
     LongPersonality.relaxed:    [1.65, 1.65, 1.65, 1.65, 1.70, 1.80, 1.80],  # more spread out
     LongPersonality.standard:   [1.35, 1.40, 1.35, 1.35, 1.50, 1.50, 1.50],  # balanced
-    LongPersonality.aggressive: [1.10, 1.15, 1.10, 1.10, 1.25, 1.25, 1.27],  # tighter
+    LongPersonality.aggressive: [1.06, 1.10, 1.06, 1.12, 1.25, 1.25, 1.27],  # tighter
 }
 FOLLOW_BREAKPOINTS =            [0.,   1.5,  3.,   6.,   12.,  16.,  36.]
 
