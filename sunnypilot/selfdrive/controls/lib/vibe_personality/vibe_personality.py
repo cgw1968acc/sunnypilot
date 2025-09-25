1283a5178e512e370093ffeef38cf11f381e5b0e
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-09-25 16:40:59 +0800

    Update vibe_personality.py

--- sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
+++ sunnypilot/selfdrive/controls/lib/vibe_personality/vibe_personality.py
@@ -16,8 +16,8 @@ AccelPersonality = custom.LongitudinalPlanSP.AccelerationPersonality
 # Acceleration Profiles mapped to AccelPersonality (eco/normal/sport)
 MAX_ACCEL_PROFILES = {
     AccelPersonality.eco:       [2.0,  1.90,  1.60, 1.10, .500, .292, .13, .09],   # eco
-    AccelPersonality.normal:    [2.0,  1.95,  1.85, 1.22, .635, .310, .17, .12],   # normal
-    AccelPersonality.sport:     [2.0,  2.00,  1.99, 1.65, .800, .333, .24, .2],    # sport
+    AccelPersonality.normal:    [2.0,  1.95,  1.85, 1.22, .635, .33, .18, .14],   # normal
+    AccelPersonality.sport:     [2.0,  2.00,  1.99, 1.65, .800, .54, .36, .3],    # sport
 }
 MAX_ACCEL_BREAKPOINTS =         [0.,   4.,   6.,   9.,   16.,  25.,  30., 55.]
 
