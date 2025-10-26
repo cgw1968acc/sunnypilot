179beb7ab3091aef86e9c10575529e52edf35927
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-10-26 13:03:45 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -62,11 +62,11 @@ CRUISE_MAX_ACCEL = 1.6
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 2.0
+    return 3.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.5
+    return 2.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.9
+    return 1.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
