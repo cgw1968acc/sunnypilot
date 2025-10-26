305740489fba18f9f757db8593ba94656fe359ef
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-10-26 08:57:18 +0800

    Adjust eco acceleration profile value

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -15,7 +15,7 @@ AccelPersonality = custom.LongitudinalPlanSP.AccelerationPersonality
 
 # Acceleration Profiles
 MAX_ACCEL_PROFILES = {
-  AccelPersonality.eco:       [2.0,  1.99,  1.92, 1.10, .500, .33, .23, .125],   # eco
+  AccelPersonality.eco:       [2.0,  1.99,  1.92, 1.00, .500, .33, .23, .125],   # eco
   AccelPersonality.normal:    [2.0,  2.00,  1.97, 1.25, .635, .48, .31, .165],   # normal
   AccelPersonality.sport:     [2.0,  2.00,  2.00, 1.90, 1.40, .70, .42, .251],    # sport
 }
