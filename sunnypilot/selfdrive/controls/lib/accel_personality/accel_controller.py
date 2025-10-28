a8fd4e2eed1cdb9cd0e6b5d1f0df136405baf470
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-10-28 22:02:06 +0800

    Update accel_controller.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -25,7 +25,7 @@ MAX_ACCEL_BREAKPOINTS =       [0.,   4.,   6.,   9.,   16.,  25.,  30., 55.]
 # Braking Profiles
 MIN_ACCEL_PROFILES = {
   AccelPersonality.eco:    [-.0100, -.0100, -.060,  -.60,  -1.20],  # gentler braking
-  AccelPersonality.normal: [-.0000015, -.0000015, -1.2,  -1.2,  -1.20],  # normal braking
+  AccelPersonality.normal: [-.0000015, -.0000015, -.060,  -.60, -1.20],  # normal braking
   AccelPersonality.sport:  [-.0000020, -.0000020, -1.2,  -1.2,  -1.20],  # more aggressive braking
 }
 MIN_ACCEL_BREAKPOINTS =    [0.,   5.0,    11.,    14,  25.]
