366f9da8fdccba1e7f237209c590484295249a7f
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-11-12 19:37:06 +0800

    Update dynamic_follow.py

--- sunnypilot/selfdrive/controls/lib/dynamic_personality/dynamic_follow.py
+++ sunnypilot/selfdrive/controls/lib/dynamic_personality/dynamic_follow.py
@@ -16,7 +16,7 @@ LongPersonality = log.LongitudinalPersonality
 FOLLOW_PROFILES = {
   LongPersonality.relaxed:    [1.30, 1.30, 1.405, 1.40, 1.80],
   LongPersonality.standard:   [1.15, 1.15, 1.285, 1.28, 1.45],
-  LongPersonality.aggressive: [0.94, 0.94, 1.065, 1.06, 1.15],
+  LongPersonality.aggressive: [0.98, 0.98, 1.079, 1.07, 1.15],
 }
 
 FOLLOW_BREAKPOINTS =          [0.,   8.,   9,    17.,  25.]
