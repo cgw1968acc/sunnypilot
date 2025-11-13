3c844a6fb6d9f760a506f02627d1fefe0f3e5bd3
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-11-13 18:57:17 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -57,7 +57,7 @@ def get_long_tune(CP, params):
         #kiV = [0.35, 0.22]
       else:
         # optimal for corolla
-        kiBP = [3.,  5.,   14.,  36.]
+        kiBP = [3.,  6.,   14.,  36.]
         kiV = [0.46, 0.30, 0.26, 0.25]
         # balanced
         #kiBP = [0., 3., 5., 8., 10., 15., 25.]
