f8de83da8d2dbd3a879d1995216d1244ef9dac3e
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-11-23 00:03:28 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -60,7 +60,7 @@ def get_long_tune(CP, params):
         # kiBP = [0.,  12.,   20.,   27.]
         # kiV =  [0.35, 0.20, 0.168, 0.1]
         # balanced
-        kiBP = [3.,   6.,   11.,    14.]
+        kiBP = [4.,   7.,   12.,    14.]
         kiV = [0.50, 0.29, 0.26, 0.25]
         # conservative
         # kiBP = [0.,  3.,  5.,  8., 12., 25.]
