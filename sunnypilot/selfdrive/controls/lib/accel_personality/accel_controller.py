f319733dcabb527da943885f4f29313ac9fec98d
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2025-11-24 19:45:50 +0800

    Update accel_controller.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -22,11 +22,11 @@ MAX_ACCEL_BREAKPOINTS =      [0.,  4.,   6.,   9.,   16.,  25., 30., 55.]
 
 # Braking Profiles
 MIN_ACCEL_PROFILES = {
-  AccelPersonality.eco:    [-0.27, -0.27, -0.27, -1.20],
-  AccelPersonality.normal: [-0.44, -0.44, -0.44, -1.30],
-  AccelPersonality.sport:  [-0.76, -0.76, -0.76, -1.40],
+  AccelPersonality.eco:    [-0.64, -0.64, -1.20],
+  AccelPersonality.normal: [-0.70, -0.70, -1.30],
+  AccelPersonality.sport:  [-0.76, -0.76, -1.40],
 }
-MIN_ACCEL_BREAKPOINTS =    [0.,    5.,    14.,   25.] 
+MIN_ACCEL_BREAKPOINTS =    [5.,    14.,   25.] 
 
 
 DECEL_SMOOTH_ALPHA = 0.03  # Very aggressive smoothing for decel (lower = smoother)
