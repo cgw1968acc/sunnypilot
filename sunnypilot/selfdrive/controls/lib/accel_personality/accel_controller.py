1219293ebed919c59bdbbe39e4d265f20a965ca6
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-03-06 23:09:41 +0800

    Update accel_controller.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -29,9 +29,9 @@ MIN_ACCEL_PROFILES = {
   #AccelPersonality.eco:       [-0.0021, -0.2400, -0.0060, -0.0070, -0.16, -0.30, -0.76], #corolla only
   #AccelPersonality.normal:    [-0.0022, -0.2600, -0.0061, -0.0071, -0.17, -0.32, -0.81], #corolla only
   #AccelPersonality.sport:     [-0.7700, -0.2800, -0.0062, -0.0072, -0.18, -0.34, -0.86], #corolla only
-  AccelPersonality.eco:       [-0.0021, -0.2400, -0.0060, -0.0070, -0.32, -0.60, -1.00], #cross
-  AccelPersonality.normal:    [-0.0022, -0.2600, -0.0061, -0.0071, -0.34, -0.62, -1.10], #cross
-  AccelPersonality.sport:     [-0.7700, -0.2800, -0.0062, -0.0072, -0.36, -0.64, -1.20], #cross
+  AccelPersonality.eco:       [-0.0021, -0.1400, -0.0060, -0.0070, -0.32, -0.60, -1.00], #cross
+  AccelPersonality.normal:    [-0.0022, -0.1600, -0.0061, -0.0071, -0.34, -0.62, -1.10], #cross
+  AccelPersonality.sport:     [-0.7700, -0.1800, -0.0062, -0.0072, -0.36, -0.64, -1.20], #cross
 }
 #MIN_ACCEL_BREAKPOINTS =       [2.0,     40.0]
 MIN_ACCEL_BREAKPOINTS =       [2.0,     4.4,    5.5,     10.0,  16.0,  22.0,  40.0]
