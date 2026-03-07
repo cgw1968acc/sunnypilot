2c2f12774c2437db1dc5144fe69e947aedb61586
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-03-07 19:47:47 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -48,13 +48,13 @@ def get_long_tune(CP, params):
       if CP.carFingerprint == CAR.TOYOTA_RAV4_TSS2:
         #optimal for rav4
         kiBP = [0.,  2.0,  4.0,  6.0,   14.]
-        kiV = [0.33, 0.35, 0.35, 0.254, 0.25]
+        kiV = [0.33, 0.35, 0.35, 0.252, 0.25]
       else:
         #kiBP = [2., 5.]
         #kiV = [0.5, 0.25]
       # test
         kiBP = [0.,  2.0,  4.0,  6.0,   14.]
-        kiV = [0.43, 0.46, 0.44, 0.252, 0.25]
+        kiV = [0.43, 0.46, 0.42, 0.254, 0.25]
     else:
       kiBP = [2., 5.]
       kiV = [0.5, 0.25]
