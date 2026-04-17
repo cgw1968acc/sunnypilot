51b538aef8cb4626cacd80a11a203e815d07be2b
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-04-17 21:18:11 +0800

    Update accel_controller.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -28,9 +28,9 @@ MIN_ACCEL_PROFILES = {
 }
 MIN_ACCEL_BREAKPOINTS =       [1.5,   3.0,   9.0,   16.0,  40.0]
 
-ACCEL_ALPHA_BASE = 0.9   # responsive for small corrections
-ACCEL_ALPHA_MAX = 0.9    # smooth for big transitions
-ACCEL_ALPHA_SCALE = 0.9   # How fast alpha grows with error
+ACCEL_ALPHA_BASE = 1.5   # responsive for small corrections
+ACCEL_ALPHA_MAX = 1.5    # smooth for big transitions
+ACCEL_ALPHA_SCALE = 1.5   # How fast alpha grows with error
 
 DECEL_ALPHA_BASE = 0.7   # smooth even for small changes
 DECEL_ALPHA_MIN = 0.7    # responsive
