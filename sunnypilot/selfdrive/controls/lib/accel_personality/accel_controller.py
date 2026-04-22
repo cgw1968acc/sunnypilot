c367d8efb924ba23d155d9e5c10782c78a36e81d
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-04-22 09:22:28 +0800

    Update accel_controller.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -22,8 +22,8 @@ MAX_ACCEL_PROFILES = {
 MAX_ACCEL_BREAKPOINTS =       [0.0,  3.0,  5.0,  8.0,  12.0, 18.0, 24.0, 32.0, 42.0]
 
 MIN_ACCEL_PROFILES = {
-  AccelPersonality.eco:       [-0.55, -0.70, -0.35, -0.35, -0.75], #cross
-  AccelPersonality.normal:    [-0.60, -0.80, -0.45, -0.45, -1.10], #cross
+  AccelPersonality.eco:       [-0.35, -0.70, -0.35, -0.35, -0.75], #cross
+  AccelPersonality.normal:    [-0.45, -0.80, -0.45, -0.45, -1.10], #cross
   AccelPersonality.sport:     [-0.90, -0.90, -0.66, -0.65, -1.20], #cross
 }
 MIN_ACCEL_BREAKPOINTS =       [1.5,   5.0,   9.0,   16.0,  40.0]
