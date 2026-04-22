30b5480417bbf46c9008ee3c991e962204a245dd
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-04-22 23:27:02 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -46,10 +46,15 @@ RIGHT_BLINDSPOT = b"\x42"
 def get_long_tune(CP, params):
   if CP.carFingerprint in TSS2_CAR:
     if Params().get_bool("ToyotaTSS2Long"):
-      #kiBP = [0.,   2.0,  9.0,  14.,  20.,  27.]
-      #kiV =  [0.25, 0.25, 0.15, 0.12, 0.12, 0.12]
-      kiBP = [2.,   5.,   12.,  20.,  35.]
-      kiV  = [0.40, 0.20, 0.12, 0.08, 0.05]
+      if CP.carFingerprint == CAR.TOYOTA_RAV4_TSS2:
+        #optimal for rav4
+        kiBP = [0.,  2.0,  4.0,  6.0,   14.]
+        kiV = [0.32, 0.32, 0.33, 0.27, 0.25]
+      else:
+        #kiBP = [1.5,  2.5,  5.0,  9.0,  27.]#cross only
+        #kiV =  [0.5,  0.50, 0.30, 0.19, 0.15]
+        kiBP = [2.,   5.,   12.,  20.,  35.]
+        kiV  = [0.40, 0.20, 0.12, 0.08, 0.05]
     else:
       kiBP = [2., 5.]
       kiV = [0.5, 0.25]
