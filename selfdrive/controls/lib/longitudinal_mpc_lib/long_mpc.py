85a738540ff7b374b12dd106d8d7526d417df604
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-05-03 10:17:19 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -60,9 +60,9 @@ CRUISE_MAX_ACCEL = 1.6
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 1.0
+    return 1.2
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.0
+    return 1.1
   elif personality==log.LongitudinalPersonality.aggressive:
     return 1.0
   else:
