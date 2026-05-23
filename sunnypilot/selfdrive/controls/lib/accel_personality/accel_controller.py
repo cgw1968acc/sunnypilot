eed0cedb967e77c68a60f954bb146cfcf8e28ee9
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-05-23 22:15:45 +0800

    Update accel_controller.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/accel_controller.py
@@ -13,12 +13,12 @@ from openpilot.common.params import Params
 AccelPersonality = custom.LongitudinalPlanSP.AccelerationPersonality
 ACCEL_PERSONALITY_OPTIONS = [AccelPersonality.eco, AccelPersonality.normal, AccelPersonality.sport]
 
-MAX_ACCEL_BP =             [0.0, 4.0, 8.0, 16., 40.0]  # m/s
+MAX_ACCEL_BP =             [0.0, 4.0, 8.0, 13., 16., 40.0]  # m/s
 
 MAX_ACCEL_V = {
-  AccelPersonality.eco:    [1.40, 1.05, 0.44, 0.15, 0.06],
-  AccelPersonality.normal: [1.80, 1.55, 0.93, 0.40, 0.13],
-  AccelPersonality.sport:  [2.00, 1.75, 1.20, 0.70, 0.20],
+  AccelPersonality.eco:    [1.50, 1.15, 0.54, 0.20, 0.15, 0.06],
+  AccelPersonality.normal: [1.80, 1.55, 0.93, 0.50, 0.40, 0.13],
+  AccelPersonality.sport:  [2.00, 1.75, 1.20, 0.80, 0.70, 0.20],
 }
 
 JERK_ACCEL_BP = [0.0,  40.0]  # m/s
