309bc194396b6bc20b6ada5aa9310c5b10c982f9
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-06-21 12:55:09 +0800

    Update kiBP and kiV values for TSS2 cars

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -46,8 +46,8 @@ RIGHT_BLINDSPOT = b"\x42"
 def get_long_tune(CP, params):
   if CP.carFingerprint in TSS2_CAR:
     if Params().get_bool("ToyotaTSS2Long"):
-      kiBP= [1.0,   5.0,   7.,  20.,  27.,  36.]
-      kiV  = [0.314, 0.223, 0.15, 0.14, 0.10, 0.09]
+      kiBP= [1.0,   2.0,   5.0,   7.,  20.,  27.,  36.]
+      kiV  = [0.314, 0.316, 0.223, 0.15, 0.14, 0.10, 0.09]
     else:
       kiBP = [2., 5.]
       kiV = [0.5, 0.25]
