f5f37ed4bf8a7bf6993fa6f7e3a3240b3cde1eaf
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-07-22 18:22:55 +0800

    Update constants.py

--- sunnypilot/selfdrive/controls/lib/accel_personality/constants.py
+++ sunnypilot/selfdrive/controls/lib/accel_personality/constants.py
@@ -24,7 +24,7 @@ PROFILE_CONFIGS = {
 ACCEL_PROFILE_MAX_BP = [0.0, 10.0, 25.0, 40.0]
 ACCEL_PROFILE_MAX_V = {
   AccelProfile.eco:  [1.45, 0.20, 0.05, 0.05], 
-  AccelProfile.normal: [1.85, 0.82, 0.25, 0.10],  
+  AccelProfile.normal: [1.85, 0.83, 0.24, 0.10],  
   AccelProfile.sport: [2.00, 1.55, 0.80, 0.50],
 }
 
