4fa705f866c1feb552c675851b0e70df4f581fda
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-08-20 20:07:33 +0800

    Update interface.py

--- opendbc_repo/opendbc/car/toyota/interface.py
+++ opendbc_repo/opendbc/car/toyota/interface.py
@@ -42,12 +42,12 @@ class CarInterface(CarInterfaceBase):
       ret.safetyConfigs[0].safetyParam |= ToyotaSafetyFlags.LTA.value
 
       # LTA control can be more delayed and winds up more often
-      ret.steerActuatorDelay = 0.18
+      ret.steerActuatorDelay = 1.1
       ret.steerLimitTimer = 0.8
     else:
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
-      ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
+      ret.steerActuatorDelay = 1.1  # Default delay, Prius has larger delay
       ret.steerLimitTimer = 0.4
 
     stop_and_go = bool(ret.flags & ToyotaFlags.TSS2)
