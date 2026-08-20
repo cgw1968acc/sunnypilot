01602d9486187470ed32e672c866a0390c282a51
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-08-20 15:26:06 +0800

    Update accel_controller.py

--- openpilot/sunnypilot/selfdrive/controls/lib/accel_controller/accel_controller.py
+++ openpilot/sunnypilot/selfdrive/controls/lib/accel_controller/accel_controller.py
@@ -22,14 +22,14 @@ MAX_ACCEL_PROFILES = {
 MAX_ACCEL_BREAKPOINTS = [0., 3., 5., 8., 12., 18., 24., 32., 42., 55.]
 
 MIN_ACCEL_PROFILES = {
-  AccelProfile.eco:    [-0.90, -0.95, -1.00, -1.10, -1.2],
-  AccelProfile.normal: [-1.00, -1.05, -1.10, -1.20, -1.3],
-  AccelProfile.sport:  [-1.10, -1.15, -1.20, -1.30, -1.4],
+  AccelProfile.eco:    [-0.90, -1.2],
+  AccelProfile.normal: [-1.00, -1.3],
+  AccelProfile.sport:  [-1.10, -1.4],
 }
-MIN_ACCEL_BREAKPOINTS = [3., 4.5, 7., 9., 25.]
+MIN_ACCEL_BREAKPOINTS = [0., 25.]
 
 ACCEL_SMOOTH_ALPHA = 0.90
-DECEL_SMOOTH_ALPHA = 0.70
+DECEL_SMOOTH_ALPHA = 0.30
 
 class AccelController:
   def __init__(self):
