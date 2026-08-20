57a0c029fcc3a87d1932f9672127abdca1873720
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-08-20 16:57:18 +0800

    Update accel_controller.py

--- openpilot/sunnypilot/selfdrive/controls/lib/accel_controller/accel_controller.py
+++ openpilot/sunnypilot/selfdrive/controls/lib/accel_controller/accel_controller.py
@@ -22,11 +22,11 @@ MAX_ACCEL_PROFILES = {
 MAX_ACCEL_BREAKPOINTS = [0., 3., 5., 8., 12., 18., 24., 32., 42., 55.]
 
 MIN_ACCEL_PROFILES = {
-  AccelProfile.eco:    [-1.00, -1.2],
-  AccelProfile.normal: [-1.00, -1.3],
-  AccelProfile.sport:  [-1.00, -1.4],
+  AccelProfile.eco:    [-0.60, -1.2],
+  AccelProfile.normal: [-0.70, -1.3],
+  AccelProfile.sport:  [-0.80, -1.4],
 }
-MIN_ACCEL_BREAKPOINTS = [0., 20.]
+MIN_ACCEL_BREAKPOINTS = [0., 25.]
 
 ACCEL_SMOOTH_ALPHA = 0.90
 DECEL_SMOOTH_ALPHA = 0.40
