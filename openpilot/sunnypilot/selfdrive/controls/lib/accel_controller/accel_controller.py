466343915db0d21dae740937bb91b2a318b894f7
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-09-21 22:25:02 +0800

    Update accel_controller.py

--- openpilot/sunnypilot/selfdrive/controls/lib/accel_controller/accel_controller.py
+++ openpilot/sunnypilot/selfdrive/controls/lib/accel_controller/accel_controller.py
@@ -16,7 +16,7 @@ AccelProfile = custom.LongitudinalPlanSP.AccelController.Profile
 MAX_ACCEL_BREAKPOINTS = [0., 3., 12,  24., 36.]  # m/s
 MAX_ACCEL_PROFILES = {
   AccelProfile.eco:    [1.80, 1.55, 0.35, 0.13, 0.10],
-  AccelProfile.normal: [1.90, 1.80, 0.60, 0.36, 0.25],
+  AccelProfile.normal: [1.90, 1.80, 0.70, 0.36, 0.25],
   AccelProfile.sport:  [2.00, 2.00, 1.20, 0.70, 0.50],
 }
 CRUISE_DECEL_RESPONSE_TIME = {  # seconds
