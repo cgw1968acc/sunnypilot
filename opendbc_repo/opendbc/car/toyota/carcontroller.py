fb54da66fc59d157027fac0d8d473107fd042ef3
cgw1968acc <156587613+cgw1968acc@users.noreply.github.com>
2026-09-25 22:50:45 +0800

    Update carcontroller.py

--- opendbc_repo/opendbc/car/toyota/carcontroller.py
+++ opendbc_repo/opendbc/car/toyota/carcontroller.py
@@ -44,8 +44,8 @@ CRUISE_CANCEL_DELAY_FRAMES = 10
 def get_long_tune(CP, CP_SP, params):
   if CP.flags & ToyotaFlags.TSS2:
     if CP_SP.flags & ToyotaFlagsSP.TSS2_LONG_TUNING:
-      kiBP = [0.0,  0.8,  5.0,  8.3,  27.]
-      kiV  = [0.50, 0.53, 0.25, 0.21, 0.10]
+      kiBP = [0.0,  1.2,  5.0,  8.3,  27.]
+      kiV  = [0.50, 0.52, 0.25, 0.21, 0.10]
     else:
       kiBP = [2., 5.]
       kiV = [0.5, 0.25]
